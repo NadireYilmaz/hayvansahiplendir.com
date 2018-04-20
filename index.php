144f4c831c288bbe8de78685b4fd0cb89b61a67b
Hatice Ergün <hadeyici@gmail.com>
2018-04-20 13:37:15 +0300

    ilk dosyalar oluşturuldu

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hayvan Sahiplendir</title>
+	<script type="text/javascript" src="sweetalert.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="sweetalert.css">
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+Merhaba 
+</body>
+</html>
