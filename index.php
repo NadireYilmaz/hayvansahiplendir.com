acb3440efba816a9546cb38729f27378f786447c
Hatice Ergün <hadeyici@gmail.com>
2018-04-20 16:17:54 +0300

    style dosyası düzenlendi

--- index.php
+++ index.php
@@ -9,6 +9,54 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-Merhaba 
+	<div id="tumalan">
+ 
+			<div id="genel">
+			<div class="alan"><img src="images/dog.png"></div>
+			<div class="alan2">
+				<span class="menu-link"><a href="sahiplendir.php">Sahiplendir</a></span>
+      			<span class="menu-link"><a href="girisyap.php">Giriş Yap</a></span>
+      			<span class="menu-link"><a href="uyeol.php">Kayıt Ol</a></span></div>
+			<div class="temizle"></div>
+		</div>
+ 
+		<div id="ortaalan">
+			<div class="yanmenu">
+				<table class="tablo">
+					<tr><a href=filtrele.php>Filtrele</a></tr>
+					<tr><td style="font-weight: bold;">Hayvanlar</td></tr>
+					<tr><td><input type="checkbox" name="">Köpek</td></tr>
+					<tr><td><input type="checkbox" name="">Kedi</td></tr>
+					<tr><td><input type="checkbox" name="">Kuş</td></tr>
+					<tr><td><select name="">
+								<option value="">Diğerleri</option>
+    							<option value="">Kaplumbağa</option>
+   								<option value="">Balık</option>
+    							<option value="">Yılan</option>
+							</select></td></tr>
+					<tr><td style="font-weight: bold;">Şehir</td></tr>
+					<tr><td><input type="checkbox" name="">İstanbul</td></tr>
+					<tr><td><input type="checkbox" name="">Ankara</td></tr>
+					<tr><td><input type="checkbox" name="">İzmir</td></tr>
+					<tr><td><input type="checkbox" name="">Bursa</td></tr>
+					<tr><td><input type="checkbox" name="">Mersin</td></tr>
+					<tr><td><select name="">
+								<option value="">Diğerleri</option>
+    							<option value="">Adana</option>
+   								<option value="">Adıyaman</option>
+    							<option value="">Afyon</option>
+							</select></td></tr>				
+				</table>
+			</div>
+			<div class="icerik"></div>
+			<div class="temizle"></div>
+		</div>
+ 
+		<div id="genel">
+			<div class="alan3">1.alan 210x50</div>
+			<div class="temizle"></div>
+		</div>
+		
+	</div>
 </body>
 </html>
